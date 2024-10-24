2df03f29c924fe63c33bff33a9cd14685d1d36cf
zhelezovartem <zhelezovartem@gmail.com>
2024-10-24 20:05:55 +0300

    cleanup

--- download_reporter_package.tf
+++ download_reporter_package.tf
@@ -1,6 +1,5 @@
 locals {
-  # package_url = "${var.reporter_releases_host}/kosli_lambda_${var.kosli_cli_version}.zip"
-  package_url = "${var.reporter_releases_host}/kosli_lambda_${var.kosli_cli_version}_test.zip"
+  package_url = "${var.reporter_releases_host}/kosli_lambda_${var.kosli_cli_version}.zip"
   downloaded  = "downloaded_package_${md5(local.package_url)}.zip"
 }
 
